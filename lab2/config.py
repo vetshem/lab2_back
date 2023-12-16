c99948a6f95a402650e52a5d83ca3d6ad4c95a02
vetshem <vetshem69@gmail.com>
2023-12-16 05:30:37 +0200

    add confuguration file

---
+++ lab2/config.py
@@ -0,0 +1,16 @@
+import os
+
+PROPAGATE_EXCEPTIONS = True
+FLASK_DEBUG = True
+
+# Database Configuration
+SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# OpenAPI Configuration
+API_TITLE = "Finance REST API"
+API_VERSION = "v1"
+OPENAPI_VERSION = "3.0.3"
+OPENAPI_URL_PREFIX = "/"
+OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
